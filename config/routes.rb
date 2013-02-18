5bdbb432c09db1b5b2fac275f80789071cdf3fcb
Dvxam <dvxam@laposte.net>
2013-02-18 17:33:17 +0100

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Writesomething::Application.routes.draw do
+  
+
+  root :to => 'users#index'
+  resources :user_sessions
+  resources :users
+  resources :password_resets
+
+  match 'login' => 'user_sessions#new', :as => :login
+  match 'logout' => 'user_sessions#destroy', :as => :logout
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
